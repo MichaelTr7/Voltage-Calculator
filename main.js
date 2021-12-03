575730b9e3fab620b9cb6e0d51b3f59f68413635
MichaelTr7 <mailmichaeltr@gmail.com>
2021-12-03 11:43:15 -0500

    Update main.js

--- main.js
+++ main.js
@@ -5,28 +5,9 @@ window.onload = function(){
     document.getElementById("Input_Voltage_Field").value = 0.5;
     document.getElementById("Plus_Button").addEventListener("click",Increment_Voltage);
     document.getElementById("Minus_Button").addEventListener("click",Decrement_Voltage);
-    document.getElementById("Plus_Button").addEventListener("touchstart",Pressed_Button);
-    document.getElementById("Minus_Button").addEventListener("touchstart",Pressed_Button);
-    document.getElementById("Plus_Button").addEventListener("touchend",Released_Button);
-    document.getElementById("Minus_Button").addEventListener("touchend",Released_Button);
+   
 }
 
-
-
-function Pressed_Button(){
-  this.style.backgroundColor = "rgb(29,79,57)";
-  this.style.transform = "scale(1.2)";
-
-  
-}
-
-function Released_Button(){
-  this.style.backgroundColor = "rgb(18,18,18)";
-  this.style.transform = "scale(1)";
-
-  
-  
-}
 
 function Voltage_Entered(){
     console.log("Voltage Entered");
@@ -39,9 +20,9 @@ function Increment_Voltage(){
     console.log("Add 6.25mV");
     let Current_Voltage = document.getElementById("Input_Voltage_Field").value;
     document.getElementById("Input_Voltage_Field").value = Math.round((parseFloat(Current_Voltage)+0.00625)*1000000)/1000000;
-    // this.classList.remove("Pulse_Button_Animation");
-    // void this.offsetWidth;
-    // this.classList.add("Pulse_Button_Animation");
+    this.classList.remove("Pulse_Button_Animation");
+    void this.offsetWidth;
+    this.classList.add("Pulse_Button_Animation");
 } 
 
 
@@ -50,7 +31,7 @@ function Decrement_Voltage(){
     let Current_Voltage = document.getElementById("Input_Voltage_Field").value;
     console.log(Current_Voltage);
     document.getElementById("Input_Voltage_Field").value = Math.round((parseFloat(Current_Voltage)-0.00625)*1000000)/1000000;
-    // this.classList.remove("Pulse_Button_Animation");
-    // void this.offsetWidth;
-    // this.classList.add("Pulse_Button_Animation");
+    this.classList.remove("Pulse_Button_Animation");
+    void this.offsetWidth;
+    this.classList.add("Pulse_Button_Animation");
 }
