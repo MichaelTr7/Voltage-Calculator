9b457d4b1d3ff4c21521bc39faa8436ab6a0e74c
Michael <67344799+MichaelTr7@users.noreply.github.com>
2022-04-04 16:48:39 -0400

    Updated voltage increment

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ self.addEventListener('install', function(event) {
 });
 
 
-
+const Increment=0.005;
 
 window.onload = function(){
     document.getElementById("Input_Voltage_Field").addEventListener("change",Voltage_Entered);
@@ -27,14 +27,14 @@ window.onload = function(){
 function Voltage_Entered(){
     console.log("Voltage Entered");
     let Voltage_Entered = this.value;
-    let Rounded_Voltage = Math.round((Math.round(Voltage_Entered/0.00625)*0.00625)*1000000)/1000000;
+    let Rounded_Voltage = Math.round((Math.round(Voltage_Entered/Increment)*Increment)*1000000)/1000000;
     this.value = Rounded_Voltage;
 }
 
 function Increment_Voltage(){
-    console.log("Add 6.25mV");
+    console.log("Add 0.05mV");
     let Current_Voltage = document.getElementById("Input_Voltage_Field").value;
-    document.getElementById("Input_Voltage_Field").value = Math.round((parseFloat(Current_Voltage)+0.00625)*1000000)/1000000;
+    document.getElementById("Input_Voltage_Field").value = Math.round((parseFloat(Current_Voltage)+Increment)*1000000)/1000000;
     this.classList.remove("Pulse_Button_Animation");
     void this.offsetWidth;
     this.classList.add("Pulse_Button_Animation");
@@ -42,10 +42,10 @@ function Increment_Voltage(){
 
 
 function Decrement_Voltage(){
-    console.log("Subtract 6.25mV");
+    console.log("Subtract 0.05mV");
     let Current_Voltage = document.getElementById("Input_Voltage_Field").value;
     console.log(Current_Voltage);
-    document.getElementById("Input_Voltage_Field").value = Math.round((parseFloat(Current_Voltage)-0.00625)*1000000)/1000000;
+    document.getElementById("Input_Voltage_Field").value = Math.round((parseFloat(Current_Voltage)-Increment)*1000000)/1000000;
     this.classList.remove("Pulse_Button_Animation");
     void this.offsetWidth;
     this.classList.add("Pulse_Button_Animation");
